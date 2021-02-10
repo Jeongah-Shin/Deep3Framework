7c096e01c05d79271eb3797202c3ecf7b7a9829d
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-02-10 10:45:40 +0900

    Add tanh class and visualize differential(backprop) process

--- deep3framework/functions.py
+++ deep3framework/functions.py
@@ -22,3 +22,14 @@ class Cos(Function):
         return gx
 def cos(x):
     return Cos()(x)
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y*y)
+        return gx
+def tanh(x):
+    return Tanh()(x)
