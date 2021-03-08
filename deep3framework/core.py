9a00b3da400747e275457cc854cb71acaf90ce71
Jeongah-Shin <jeongah.h.shin@gmail.com>
2021-03-09 01:47:03 +0900

    Modify Add/Mul/Sub/Div class to deal with broadcast

--- deep3framework/core.py
+++ deep3framework/core.py
@@ -161,19 +161,30 @@ class Square(Function):
 
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape: # for broadcast
+            gx0 = deep3framework.functions.sum_to(gx0, self.x0_shape)
+            gx1 = deep3framework.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 class Mul(Function):
     def forward(self, x0, x1):
         y = x0 * x1
         return y
+
     def backward(self, gy):
         # x0, x1 = self.inputs[0].data, self.inputs[1].data
         x0, x1 = self.inputs
-        return gy * x1, gy * x0
+        gx0 = gy * x1
+        gx1 = gy * x0
+        if x0.shape != x1.shape: # for broadcast
+            gx0 = deep3framework.functions.sum_to(gx0, x0.shape)
+            gx1 = deep3framework.functions.sum_to(gx1, x1.shape)
+        return gx0, gx1
 
 class Neg(Function):
     def forward(self, x):
@@ -183,20 +194,30 @@ class Neg(Function):
 
 class Sub(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 - x1
         return y
     def backward(self, gy):
-        return gy, -gy
+        gx0 = gy
+        gx1 = -gy
+        if self.x0_shape != self.x1_shape: # for broadcast
+            gx0 = deep3framework.functions.sum_to(gx0, self.x0_shape)
+            gx1 = deep3framework.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 class Div(Function):
     def forward(self, x0, x1):
         y = x0 / x1
         return y
+
     def backward(self, gy):
         # x0, x1 = self.inputs[0].data, self.inputs[1].data
         x0, x1 = self.inputs
         gx0 = gy / x1
         gx1 = gy * (-x0 / x1 ** 2)
+        if x0.shape != x1.shape: # for broadcast
+            gx0 = deep3framework.functions.sum_to(gx0, x0.shape)
+            gx1 = deep3framework.functions.sum_to(gx1, x1.shape)
         return gx0, gx1
 
 class Pow(Function):
